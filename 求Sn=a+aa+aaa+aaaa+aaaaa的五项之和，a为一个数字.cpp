b7610da83e3011467714c568a3aef0e5e5161f12
Amnesia-E <105757795+Amnesia-E@users.noreply.github.com>
2022-05-27 23:09:01 +0800

    Add files via upload

---
+++ 求Sn=a+aa+aaa+aaaa+aaaaa的五项之和，a为一个数字.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int n = 0;
+	scanf("%d%d", &a,&n);//2 5
+	int sun = 0;
+	int i = 0;
+	int ret = 0;
+	//2 22 222 2222
+	for (i = 0; i < n; i++)
+	{
+		ret =ret * 10 + a;
+		sun = ret;
+	}
+	printf("%d", sun);
+
+
+	return 0;
+}
